224b817591863628c29564bb8f69d02d0b2de09f
Justin Gries <justin.gries@gmail.com>
2021-10-15 22:22:16 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ * Course: Chaffey College 2021-Fall CS2
+ * Prof: Dr. Tracy Kocher
+ * Date:
+ * Project:
+ * Coder: Justin Gries
+ * Tester(s):
+ *
+ * Prompt:
+ */
+int main(int argc, char **argv) {
+    cout << "Hello 7.02" << endl;
+    return 0;
+}
