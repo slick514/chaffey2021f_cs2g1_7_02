8b9bd7dcb58ebef2701607b0341c335dd89c67f8
Justin Gries <justin.gries@gmail.com>
2021-10-16 19:23:12 -0700

    I'm a terrible git user. WAY too many changes. The UI is working up through accepting a usernumber from the end-user

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 /*
@@ -25,26 +26,37 @@ using namespace std;
  */
 
 // Program welcome message
-const string APP_NAME = "CopyMachine 7.02";
+static string APP_NAME = "CopyMachine 7.02"; // NOLINT
 
 // User input options (lower case)
-const char USER = 'u';
-const char ADMIN = 'a';
-const char QUIT = 'q';
-const char BALANCE = 'b';
-const char MASTER_ACCT = 'm';
-const char PROJECT = 'p';
-const char COPIES = 'c';
+static const char USER = 'U'; // NOLINT
+static const char ADMIN = 'A'; // NOLINT
+static const char QUIT = 'Q'; // NOLINT
+static const char BALANCE = 'B'; // NOLINT
+static const char MASTER_ACCT = 'M'; // NOLINT
+static const char PROJECT = 'P'; // NOLINT
+static const char COPIES = 'C'; // NOLINT
+static const char EXIT = 'E'; // NOLINT
 
-const int NUMBER_OF_USERS = 100;
-const int NUMBER_OF_ACCOUNTS = 10;
+static const int NUMBER_OF_USERS = 100;
+static const int NUMBER_OF_ACCOUNTS = 10;
+static const int BASE_10 = 10;
 
-void enter_control_loop(int **users, int *accounts, int master_acct);
-void do_user_UI();
-void do_admin_UI();
-void display_invalid_input_message(char);
-void do_greeting();
-void do_exit_message();
+void enter_control_loop(int**, int*, int);
+void do_user_UI(bool&);
+void do_admin_UI(bool&);
+void display_invalid_input_message(string&);
+void display_application_welcome_msg();
+void display_exit_message();
+void detect_range(const string &int_string, const int &max, bool &is_out_of_range);
+void handle_nonint_int_input(string user_input, bool &is_invalid, bool &nav_main_flag, bool &quit_flag);
+void detect_int(const string &str, bool &);
+void display_out_of_range_err_msg(const int&, const int&);
+void display_usernum_prompt();
+void display_main_menu_prompt();
+void display_usermenu_welcome();
+void obtain_int_from_UI(int &int_ref, int, bool&, bool &);
+void obtain_usernum(int &num_ref, bool&, bool&);
 
 int main(int argc, char **argv) {
     // initialize user and account arrays
@@ -53,65 +65,140 @@ int main(int argc, char **argv) {
     int project_accounts[NUMBER_OF_ACCOUNTS];
     for (auto &user: users) user = &master_account;
 
-    do_greeting();
+    display_application_welcome_msg();
     enter_control_loop(users, project_accounts, master_account);
-    do_exit_message();
+    display_exit_message();
+
     return 0;
 }
 
 
 void enter_control_loop(int **users, int *accounts, int master_acct) {
     char choice = QUIT;
+    string input_string;
+    bool quit_flag = false;
+
     do{
-        cout << "Please choose from the following options:" << endl;
-        cout << "'" << (char)toupper(USER) << "' if you are a User" << endl;
-        cout << "'" << (char)toupper(ADMIN) << "' if you are an Administrator" << endl;
-        cout << "'" << (char)toupper(QUIT) << "' to Quit" << endl;
-        cin >> choice;
-        choice = (char) tolower(choice);
+        display_main_menu_prompt();
+        cin >> input_string;
+        choice = (char)toupper(input_string[0]);
         switch(choice){
             case USER:
-                do_user_UI();
+                do_user_UI(quit_flag);
                 break;
             case ADMIN:
-                do_admin_UI();
+                do_admin_UI(quit_flag);
                 break;
             case QUIT:
                 break;
             default:
-                display_invalid_input_message(choice);
+                display_invalid_input_message(input_string);
         }
-    }while(choice != QUIT);
+    }while(!(choice == QUIT || quit_flag));
 }
 
+void display_main_menu_prompt() {
+    cout << "Please choose from the following options:" << endl;
+    cout << "(" << USER << ") if you are a User" << endl;
+    cout << "(" << ADMIN << ") if you are an Administrator" << endl;
+    cout << "(" << QUIT << ") to Quit" << endl;
+}
 
-void do_greeting(){
+void display_application_welcome_msg(){
     cout << "Welcome to " << APP_NAME << endl << endl;
 }
 
-
-void do_exit_message() {
-    cout << endl << "Thank you for using " << APP_NAME << endl << "Goodbye!" <<
-    endl;
+void display_exit_message() {
+    cout << endl;
+    cout << "Thank you for using " << APP_NAME << endl;
+    cout << "Goodbye!" << endl;
 }
 
-
-void display_invalid_input_message(char invalid_input) {
-    cout << endl << "'" << invalid_input << "' is not a valid input. ";
-    cout << "Please try again." << endl;
+void display_invalid_input_message(string &invalid_input) {
+    cout << endl ;
+    cout << "'" << invalid_input << "' is not a valid input. Please try again." << endl;
 }
 
 // TODO:
-void do_admin_UI() {
+void do_admin_UI(bool &quit_flag) {
     char user_choice;
     //cin >> user_choice;
 }
 
 // TODO:
-void do_user_UI() {
-    char user_choice;
+void do_user_UI(bool &quit_flag) {
+    int user_number;
+    bool nav_main_flag;
+
+    display_usermenu_welcome();
+    obtain_usernum(user_number, nav_main_flag, quit_flag);
+}
+
+void obtain_int_from_UI(int &int_ref, int range, bool &nav_main_flag, bool &quit_flag) {
+    bool is_invalid = false;
+    do {
+        bool is_int;
+        string user_input;
+        cin >> user_input;
+        detect_int(user_input, is_int);
+
+        if (!is_int) { // input is non-integer
+            handle_nonint_int_input(user_input, is_invalid, nav_main_flag, quit_flag);
+        }
+        else if(range > 0) // We now  know it's an integer; check if it is in range
+            detect_range(user_input, range, is_invalid);
+
+    }while(is_invalid && !nav_main_flag);
+}
+
+void obtain_usernum(int &usernum, bool &nav_main_flag, bool &quit_flag) {;
+    display_usernum_prompt();
+    obtain_int_from_UI(usernum, NUMBER_OF_USERS, nav_main_flag, quit_flag);
+
+}
+
+void display_usermenu_welcome() {
     cout << "Welcome to the User menu." << endl;
-    cout << "Please enter your user number: ";
-    cin >> user_choice;
-    //cin >> user_choice;
+}
+
+void display_usernum_prompt() {
+
+    cout << "Please enter one of the following:" << endl;
+    cout << "\tYour user-number:" << endl;
+    cout << "\t(" << EXIT << ") to exit to the main menu" << endl;
+    cout << "\t(" << QUIT << ") to quit the application." << endl;
+}
+
+void display_out_of_range_err_msg(const int &max_range, const int &range) {
+    cout << "The number you entered (" << max_range << ") is not within range (1 to "<< range <<"). Please enter an ";
+    cout << "appropriate value." << endl;
+}
+
+// Detects if the string is an integer.
+void detect_int(const string &str, bool &is_int) {
+    for (char i : str) {
+        if (!isdigit(i)){
+            is_int = false;
+            return;
+        }
+    }
+    is_int = true;
+}
+
+void handle_nonint_int_input(string user_input, bool& is_invalid, bool &nav_main_flag, bool &quit_flag) {
+    char uc_input_char = (char) toupper(user_input[0]);
+    nav_main_flag = uc_input_char == EXIT;
+    quit_flag = uc_input_char == QUIT;
+    if(!quit_flag && !nav_main_flag)
+        display_invalid_input_message(user_input);
+}
+
+void detect_range(const string &int_string, const int &max, bool &is_out_of_range) {
+    char *end;
+    int user_number = strtol(int_string.c_str(), &end, BASE_10);
+    if ((user_number > 0) && (user_number <= max)){
+        is_out_of_range = false;
+    }else{
+        display_out_of_range_err_msg(user_number, max);
+    }
 }
